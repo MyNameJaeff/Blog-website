48b24b7bb7da97c98e408d788935a6fe59bdefe8
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-05-02 13:38:37 +0200

    files

---
+++ blogs/rat.php
@@ -0,0 +1,12 @@
+<?php include '../php/header.php'; ?>
+    <main>
+    <div>
+    <a href='../php/index.php'>Go back</a>
+    </div>
+    <div><h1 class='titleofablog'>rat</h1>
+<h3 class='authorofablog'>test</h3>
+<p class='textofablog'>rat</p>
+<img src='../uploads/pepe_groove.gif' alt='uploadedimage' width='200px' class='imageofablog'>
+</div>
+    </main>
+    <?php include '../php/footer.php'; ?>
