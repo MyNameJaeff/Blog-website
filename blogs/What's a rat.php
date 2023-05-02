8a4f857e6aa7dc532d6221c2dfaafe76d82bdf57
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-05-02 19:14:53 +0200

    rem

--- blogs/What's a rat.php
+++
@@ -1,9 +0,0 @@
-<?php include '../php/header.php'; ?>
-    <main>
-    <div><h1 class='titleofablog'>What's a rat</h1>
-<h3 class='authorofablog'>test</h3>
-<p class='textofablog'>A rat is an animal with a tail and stuff</p>
-<img src='../uploads/backupimage.png' alt='uploadedimage' width='200px' class='imageofablog'>
-</div>
-    </main>
-    <?php include '../php/footer.php'; ?>
