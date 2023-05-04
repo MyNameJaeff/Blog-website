6baaf3a87a88700b381480870b8bfde11781ba49
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-05-04 09:58:34 +0200

    reset

--- blogs/This is a title.php
+++
@@ -1,9 +0,0 @@
-<?php include '../php/header.php'; include '../php/blogposter.php';?>
-    <main>
-    <div><h1 class='titleofablog'>This is a title</h1>
-<h3 class='authorofablog'>Username</h3>
-<p class='textofablog'>This is the text thats going to be inside the blog page</p>
-<img src='../uploads/coep.webp' alt='uploadedimage' width='200px' class='imageofablog'>
-</div>
-    </main>
-    <?php include '../php/footer.php'; ?>
