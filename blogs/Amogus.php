8baa2b2613b15bf60c042a4740227d78ab868416
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-05-04 14:33:58 +0200

    hbkjmn

---
+++ blogs/Amogus.php
@@ -0,0 +1,9 @@
+<?php include '../php/header.php'; include '../php/blogposter.php';?>
+    <main>
+    <div><h1 class='titleofablog'>Amogus</h1>
+<h3 class='authorofablog'>Evelinn Sjöström</h3>
+<p class='textofablog'>There is a sussy baka going around the neighborhood</p>
+<img src='../uploads/amogus.png' alt='uploadedimage' width='200px' class='imageofablog'>
+</div>
+    </main>
+    <?php include '../php/footer.php'; ?>
